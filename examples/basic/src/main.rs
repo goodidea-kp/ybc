703f273fb5bb45be92101d382c825691514877e5
kostya <konstantin.pupkov@fromkos.com>
2025-07-21 18:50:40 -0700

    cleanup formatting in examples/basic/src/main.rs

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,13 +1,12 @@
 #![recursion_limit = "1024"]
 
-use std::rc::Rc;
 use console_error_panic_hook::set_once as set_panic_hook;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use ybc::TileCtx::{Ancestor, Child, Parent};
 use yew::prelude::*;
 
-
-use ybc::{ NavBurgerCloserState};
+use ybc::NavBurgerCloserState;
 
 #[function_component(App)]
 pub fn app() -> Html {
@@ -111,17 +110,13 @@ fn main() {
     yew::Renderer::<App>::new().render();
 }
 
-
-use ybc::ModalCloserProvider;
 use ybc::ModalCloserContext;
-
+use ybc::ModalCloserProvider;
 
 #[function_component]
 pub fn MyModal1() -> Html {
     let msg_ctx = use_context::<ModalCloserContext>().unwrap();
-    let onclick = {
-        Callback::from(move |e:MouseEvent| msg_ctx.dispatch( "id0-close".to_string().parse().unwrap()))
-    };
+    let onclick = { Callback::from(move |e: MouseEvent| msg_ctx.dispatch("id0-close".to_string().parse().unwrap())) };
     let on_click_cb = Callback::from(move |e: AttrValue| {
         gloo_console::log!("Button clicked!");
     });
@@ -160,13 +155,9 @@ pub fn MyModal1() -> Html {
 #[function_component]
 pub fn MyModal2() -> Html {
     let msg_ctx = use_context::<ModalCloserContext>().unwrap();
-    let onclick = {
-        Callback::from(move |e:MouseEvent| msg_ctx.dispatch( "id2-close".to_string().parse().unwrap()))
-    };
+    let onclick = { Callback::from(move |e: MouseEvent| msg_ctx.dispatch("id2-close".to_string().parse().unwrap())) };
     let msg_ctx2 = use_context::<ModalCloserContext>().unwrap();
-    let onsave = {
-        Callback::from(move |e:MouseEvent| msg_ctx2.dispatch( "id2-close".to_string().parse().unwrap()))
-    };
+    let onsave = { Callback::from(move |e: MouseEvent| msg_ctx2.dispatch("id2-close".to_string().parse().unwrap())) };
     let on_click_cb = Callback::from(move |e: AttrValue| {
         gloo_console::log!("Button clicked!");
     });
